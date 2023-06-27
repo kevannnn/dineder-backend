b6d6eaae542ee36a111e786520d2571de67c3e20
kevannnn <e0957921@u.nus.edu>
2023-06-28 05:55:44 +0800

    backend updates

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	r := router.Setup()
-	fmt.Print("Listening on port 8000 at http://localhost:8000!")
+	fmt.Print("Listening on port 3000 at http://localhost:3000!")
 
-	log.Fatalln(http.ListenAndServe(":8000", r))
+	log.Fatalln(http.ListenAndServe(":3000", r))
 }
--- internal/router/router.go
+++ internal/router/router.go
@@ -3,10 +3,22 @@ package router
 import (
 	"github.com/kevannnn/dineder-backend/internal/routes"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 )
 
 func Setup() chi.Router {
 	r := chi.NewRouter()
+	
+	corsMiddleware := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:19006"}, 
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	})
+
+	r.Use(corsMiddleware.Handler)
 	setUpRoutes(r)
 	return r
 }
